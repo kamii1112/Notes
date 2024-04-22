b40d8c41063b8300785c3fee9ac93ea270d6820a
JellaKomal <komalkumar0412@gmail.com>
2024-04-22 19:07:42 +0530

    adding html and css files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HTML from "./HTML";
+import CSS from "./CSS";
+
+function App() {
+    return (
+        <div>
+            <HTML/>
+            <CSS/>
+        </div>
+    );
+}
+
+export default App;
